36f6cc397258202f676f1c94972089ed077d1db3
sajithaliyanage <sajithaliyanage@gmail.com>
2018-08-08 22:38:23 +0530

    adding routes

--- chainkeeper_app/src/components/App.js
+++ chainkeeper_app/src/components/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from '../public/logo.svg';
+import { BrowserRouter as Router,Route, } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import * as routes from '../constants/routes';
+import Setup from './Setup';
+
+const App = () =>
+  <Router>
+    <div>
+      <Route
+        exact path={routes.LANDING}
+        component={() => <Setup />}
+      />
+    </div>
+  </Router>
 
 export default App;
