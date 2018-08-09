4d1947d3cc839a3fc82d4bab4ae0daa3d2848759
sajithaliyanage <sajithaliyanage@gmail.com>
2018-08-09 08:56:40 +0530

    updated nav bar

--- chainkeeper_app/src/components/Navigation.js
+++ chainkeeper_app/src/components/Navigation.js
@@ -14,26 +14,26 @@ const Navigation = () =>
   </AuthUserContext.Consumer>
 
 const NavigationNonAuth = () =>
-    <nav className="navbar navbar-expand-lg" style={{backgroundColor:"#10767A", borderColor:"none"}}>
-        <a className="navbar-brand" href="#">ChainKeeper</a>
+    <nav className="navbar navbar-expand-lg" style={{backgroundColor:"#10767A", borderColor:"none", height:"80px"}}>
+        <a className="navbar-brand" href="#" style={{color:"#ffffff"}}><b>CHAIN_KEEPER</b></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"/>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                <li className="nav-item item-hover active" style={{margin: "0 20px 0 20px"}}>
+                    <a className="nav-link" href="/" style={{color:"#ffffff"}}>HOME <span className="sr-only">(current)</span></a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
+                <li className="nav-item item-hover" style={{margin: "0 10px 0 10px"}}>
+                    <a className="nav-link" href="/explorer" style={{color:"#ffffff"}}>DATA</a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
+                <li className="nav-item item-hover" style={{margin: "0 20px 0 10px"}}>
+                    <a className="nav-link" href="/signin" style={{color:"#ffffff"}}>SIGN IN</a>
                 </li>
             </ul>
-             <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" />
+             <form className="form-inline my-2 my-lg-1">
+              <input className="form-control mr-sm-2" type="search" style={{marginLeft:"-40px !important", width:"270px"}} placeholder="BLOCK, HASH, TRANSACTION ..." />
               <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
