0a6caded3d02636e27776e7e6cdec0dafc04494e
sajithaliyanage <sajithaliyanage@gmail.com>
2018-08-12 14:48:48 +0530

    fix isse of block height boundry in api

--- chainkeeper_api/services/blockchain.py
+++ chainkeeper_api/services/blockchain.py
@@ -5,24 +5,29 @@ chain = blocksci.Blockchain("/root/bitcoin-data/blocksci-514496-v5")
 
 #get block data function with block height
 def getBlockData(block_height):
-    blockData = chain.blocks[block_height]
     response = {
         "data": {},
         "status": "success"
     }
 
-    blockData = {
-        "height": blockData.height,
-        "block_hash": str(blockData.hash),
-        "output_value": (blockData.output_value/100000000),
-        "numTxes": blockData.tx_count,
-        "timestamp": blockData.timestamp,
-        "size": blockData.size_bytes,
-        "nonce": blockData.nonce
-    }
+    if(len(chain.blocks) < block_height):
+        response["status"] = "failed: Maximum block height is "+str(len(chain.blocks))
+        return jsonify(response)
+    else:
+        blockData = chain.blocks[block_height]
 
-    response["data"] = blockData
-    return jsonify(response)
+        blockData = {
+            "height": blockData.height,
+            "block_hash": str(blockData.hash),
+            "output_value": (blockData.output_value/100000000),
+            "numTxes": blockData.tx_count,
+            "timestamp": blockData.timestamp,
+            "size": blockData.size_bytes,
+            "nonce": blockData.nonce
+        }
+
+        response["data"] = blockData
+        return jsonify(response)
 
 #get block data function for block data range
 def getBlockRangeData(block_height1,block_height2):
@@ -37,24 +42,29 @@ def getBlockRangeData(block_height1,block_height2):
         response["status"] = "failed: Invalid block range"
         return jsonify(response)
     else:
-        element = []
-        for x in range(rangeVal + 1):
-            blockData = chain.blocks[block_height1 + x]
 
-            blockData = {
-                "height": blockData.height,
-                "block_hash": str(blockData.hash),
-                "output_value": (blockData.output_value/100000000),
-                "numTxes": blockData.tx_count,
-                "timestamp": blockData.timestamp,
-                "size": blockData.size_bytes,
-                "nonce": blockData.nonce
-            }
+        if(len(chain.blocks) > block_height2):
+            element = []
+            for x in range(rangeVal + 1):
+                blockData = chain.blocks[block_height1 + x]
 
-            element.append(blockData)
-        response["data"] = element
+                blockData = {
+                    "height": blockData.height,
+                    "block_hash": str(blockData.hash),
+                    "output_value": (blockData.output_value/100000000),
+                    "numTxes": blockData.tx_count,
+                    "timestamp": blockData.timestamp,
+                    "size": blockData.size_bytes,
+                    "nonce": blockData.nonce
+                }
 
-    return jsonify(response)
+                element.append(blockData)
+            response["data"] = element
+
+            return jsonify(response)
+        else:
+            response["status"] = "failed: Maximum block height is "+str(len(chain.blocks))
+            return jsonify(response)
 
 
 #get tx data function with hash
