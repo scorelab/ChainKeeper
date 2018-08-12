1409e78f9946a826533c2dc68d41687e7ca7ad50
sajithaliyanage <sajithaliyanage@gmail.com>
2018-08-12 15:26:34 +0530

    updated tx ins and outs elements

--- chainkeeper_api/services/tx.py
+++ chainkeeper_api/services/tx.py
@@ -1,100 +1,132 @@
 from flask import jsonify
 import blocksci
+import sys
 
 chain = blocksci.Blockchain("/root/bitcoin-data/blocksci-514496-v5")
 
 def getTxInputs(tx_hash):
-    txData = chain.tx_with_hash(tx_hash)
-    txInputs = txData.ins
-
     response = {
         "data": [],
         "status": "success"
     }
 
-    for x in range(len(txInputs)):
-        inputData = {
-            "spent_tx_index": txInputs[x].spent_tx_index,
-            "address": txInputs[x].address,
-            "value": txInputs[x].value,
-            "age": txInputs[x].age,
-            "block_height": txInputs[x].block.height,
-            "sequence_num": txInputs[x].sequence_num
-        }
+    try:
+        txData = chain.tx_with_hash(tx_hash)
+        txInputs = txData.ins
 
-        response["data"][x] = inputData
+        element = []
+        for x in range(len(txInputs)):
+            inputData = {
+                "spent_tx_index": txInputs[x].spent_tx_index,
+                "address": txInputs[x].address,
+                "value": (txInputs[x].value/100000000),
+                "age": txInputs[x].age,
+                "block_height": txInputs[x].block.height,
+                "sequence_num": txInputs[x].sequence_num
+            }
 
+            element.append(inputData)
 
-    return jsonify(response)
+        response["data"] = element
+        return jsonify(response)
+
+    except:  # catch *all* exceptions
+        e = sys.exc_info()[0]
+        response["status"] = str(e)
+
+        return jsonify(response)
 
 def getTxInputsIndex(tx_index):
-    txData = chain.tx_with_hash(tx_index)
-    txInputs = txData.ins
-
     response = {
         "data": [],
         "status": "success"
     }
 
-    for x in range(len(txInputs)):
-        inputData = {
-            "spent_tx_index": txInputs[x].spent_tx_index,
-            "address": txInputs[x].address,
-            "value": txInputs[x].value,
-            "age": txInputs[x].age,
-            "block_height": txInputs[x].block.height,
-            "sequence_num": txInputs[x].sequence_num
-        }
+    try:
+        txData = chain.tx_with_index(int(tx_index))
+        txInputs = txData.ins
 
-        response["data"][x] = inputData
+        element = []
+        for x in range(len(txInputs)):
+            inputData = {
+                "spent_tx_index": txInputs[x].spent_tx_index,
+                "address": txInputs[x].address,
+                "value": (txInputs[x].value/100000000),
+                "age": txInputs[x].age,
+                "block_height": txInputs[x].block.height,
+                "sequence_num": txInputs[x].sequence_num
+            }
+            element.append(inputData)
 
+        response["data"] = element
+        return jsonify(response)
 
-    return jsonify(response)
+    except:  # catch *all* exceptions
+        e = sys.exc_info()[0]
+        response["status"] = str(e)
+
+        return jsonify(response)
 
 def getTxOutputs(tx_hash):
-    txData = chain.tx_with_hash(tx_hash)
-    txOutputs = txData.outs
-
     response = {
         "data": [],
         "status": "success"
     }
 
-    for x in range(len(txOutputs)):
-        outputData = {
-            "is_spent": txOutputs[x].is_spent,
-            "spending_tx_index": txOutputs[x].spending_tx_index,
-            "address": txOutputs[x].address,
-            "value": txOutputs[x].value,
-            "block_height": txOutputs[x].block.height
-        }
+    try:
+        txData = chain.tx_with_hash(tx_hash)
+        txOutputs = txData.outs
 
-        response["data"][x] = outputData
+        element = []
+        for x in range(len(txOutputs)):
+            outputData = {
+                "is_spent": txOutputs[x].is_spent,
+                "spending_tx_index": txOutputs[x].spending_tx_index,
+                "address": txOutputs[x].address,
+                "value": (txOutputs[x].value/100000000),
+                "block_height": txOutputs[x].block.height
+            }
 
+            element.append(outputData)
 
-    return jsonify(response)
+        response["data"] = element
+        return jsonify(response)
+
+    except:  # catch *all* exceptions
+        e = sys.exc_info()[0]
+        response["status"] = str(e)
+
+        return jsonify(response)
 
 def getTxOutputsIndex(tx_index):
-    txData = chain.tx_with_hash(tx_index)
-    txOutputs = txData.outs
-
     response = {
         "data": [],
         "status": "success"
     }
 
-    for x in range(len(txOutputs)):
-        outputData = {
-            "is_spent": txOutputs[x].is_spent,
-            "spending_tx_index": txOutputs[x].spending_tx_index,
-            "address": txOutputs[x].address,
-            "value": txOutputs[x].value,
-            "block_height": txOutputs[x].block.height
-        }
+    try:
+        txData = chain.tx_with_index(int(tx_index))
+        txOutputs = txData.outs
 
-        response["data"][x] = outputData
+        element = []
+        for x in range(len(txOutputs)):
+            outputData = {
+                "is_spent": txOutputs[x].is_spent,
+                "spending_tx_index": txOutputs[x].spending_tx_index,
+                "address": txOutputs[x].address,
+                "value": (txOutputs[x].value/100000000),
+                "block_height": txOutputs[x].block.height
+            }
 
+            element.append(outputData)
 
-    return jsonify(response)
+        response["data"] = element
+        return jsonify(response)
+
+    except:  # catch *all* exceptions
+        e = sys.exc_info()[0]
+        response["status"] = str(e)
+
+        return jsonify(response)
 
 
